4327c98b19475259cb1061dc17d7ad2c268822f4
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-07-16 04:26:21 +0900

    OverpassAPIのリクエストheader Acceptを制限

--- src/main/java/pro/eng/yui/oss/ksj2salvage/worker/OverpassClient.java
+++ src/main/java/pro/eng/yui/oss/ksj2salvage/worker/OverpassClient.java
@@ -45,7 +45,7 @@ public class OverpassClient {
         Request request = new Request.Builder()
             .url("https://overpass-api.de/api/interpreter")
             .header("User-Agent", "KSJ2AddressSalvage/1.0 (https://github.com/yui-oss/ksj2salvage)")
-            .header("Accept", "application/osm3s+xml, application/xml, */*")
+            .header("Accept", "application/osm3s+xml")
             .header("Accept-Encoding", "gzip, deflate, br")
             .post(new okhttp3.FormBody.Builder().add("data", query).build())
             .build();
